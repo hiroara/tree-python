743561152e05eea4c78f51ddaeb530c88bdfbddc
Arai Hiroki <hiroara62@gmail.com>
2016-05-08 01:25:59 +0900

    support list data

--- tree/builder.py
+++ tree/builder.py
@@ -6,12 +6,33 @@ def build_tree(data, root_value=None):
 
 
 def __build_children(data):
-    assert type(data) is dict
-    return [__build_child(key, val) for (key, val) in sorted(data.items(), key=lambda item: item[0])]
+    if __is_as_dict(data):
+        return [__build_child(key, val) for (key, val) in sorted(data.items(), key=lambda item: item[0])]
+    elif __is_as_list(data):
+        return [__build_child(idx, val) for (idx, val) in enumerate(data)]
+    raise Exception('Unsupported data type: {}'.format(data))
 
 
 def __build_child(value, data):
-    if type(data) is dict:
+    if __is_as_tree_data(data):
         return Tree(value, __build_children(data))
     else:
         return LeafTree(value, LeafNode(data))
+
+
+def __is_as_dict(data):
+    try:
+        return callable(getattr(data, 'items'))
+    except AttributeError:
+        return False
+
+
+def __is_as_list(data):
+    try:
+        return callable(getattr(data, '__iter__'))
+    except AttributeError:
+        return False
+
+
+def __is_as_tree_data(data):
+    return __is_as_dict(data) or __is_as_list(data)
