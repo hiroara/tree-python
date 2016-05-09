aa2152fdc35100b448cca90195365018faf55792
Arai Hiroki <hiroara62@gmail.com>
2016-05-09 10:27:54 +0900

    implement PostorderWalker

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -1,24 +1,44 @@
 import pytest
 
 from tree import pretty_print
-from tree.walker import PreorderWalker
+from tree.walker import PreorderWalker, PostorderWalker
 
 
 class TestPreorderWalker:
     @pytest.fixture
-    def walker3(self, tree1):
+    def walker1(self, tree1):
         return PreorderWalker(tree1)
 
-    def test_next(self, tree1, walker3):
+    def test_next(self, tree1, walker1):
         pretty_print(tree1)
-        assert next(walker3) is tree1
-        assert next(walker3) is tree1.get('a')
-        assert next(walker3) is tree1.get('a/b')
-        assert next(walker3) is tree1.get('a/b/c')
-        assert next(walker3) is tree1.get('a/b/c').leaf
-        assert next(walker3) is tree1.get('a/d')
-        assert next(walker3) is tree1.get('a/d').leaf
-        assert next(walker3) is tree1.get('e')
-        assert next(walker3) is tree1.get('e').leaf
+        assert next(walker1) is tree1
+        assert next(walker1) is tree1.get('a')
+        assert next(walker1) is tree1.get('a/b')
+        assert next(walker1) is tree1.get('a/b/c')
+        assert next(walker1) is tree1.get('a/b/c').leaf
+        assert next(walker1) is tree1.get('a/d')
+        assert next(walker1) is tree1.get('a/d').leaf
+        assert next(walker1) is tree1.get('e')
+        assert next(walker1) is tree1.get('e').leaf
         with pytest.raises(StopIteration):
-            next(walker3)
+            next(walker1)
+
+
+class TestPostorderWalker:
+    @pytest.fixture
+    def walker1(self, tree1):
+        return PostorderWalker(tree1)
+
+    def test_next(self, tree1, walker1):
+        pretty_print(tree1)
+        assert next(walker1) is tree1.get('a/b/c').leaf
+        assert next(walker1) is tree1.get('a/b/c')
+        assert next(walker1) is tree1.get('a/b')
+        assert next(walker1) is tree1.get('a/d').leaf
+        assert next(walker1) is tree1.get('a/d')
+        assert next(walker1) is tree1.get('a')
+        assert next(walker1) is tree1.get('e').leaf
+        assert next(walker1) is tree1.get('e')
+        assert next(walker1) is tree1
+        with pytest.raises(StopIteration):
+            next(walker1)
--- tree/walker.py
+++ tree/walker.py
@@ -1,45 +1,71 @@
 from tree import Tree, LeafNode
 
 
-class PreorderWalker:
+class Walker:
     def __init__(self, tree):
-        self.tree = tree
+        self.root = tree
         self.current = tree
         self.route = []
 
+    def _walk_to(self, node):
+        self.route.append(self.current)
+        self.current = node
+
+    def _back(self):
+        self.current = self.route[-1]
+        del self.route[-1]
+
+    def _next_sibling_index(self):
+        parent = self.route[-1]
+        index = parent.children.index(self.current)
+        if len(parent.children) > index + 1:
+            return index + 1
+
+
+class PreorderWalker(Walker):
     def __next__(self):
         result = self.current
         if result is None:
             raise StopIteration()
         if isinstance(self.current, Tree):
-            self.__walk_to(self.current.children[0])
+            self._walk_to(self.current.children[0])
         elif isinstance(self.current, LeafNode):
             next_node = self.__rise()
             if next_node is None:
                 self.current = None
             else:
-                self.__walk_to(next_node)
+                self._walk_to(next_node)
         return result
-
-    def __walk_to(self, node):
-        self.route.append(self.current)
-        self.current = node
-
-    def __back(self):
-        self.current = self.route[-1]
-        del self.route[-1]
-
-    def __next_sibling_index(self):
-        parent = self.route[-1]
-        index = parent.children.index(self.current)
-        if len(parent.children) > index + 1:
-            return index + 1
 
     def __rise(self):
         next_sibling_index = None
         while next_sibling_index is None:
             if not self.route:
                 return None
-            next_sibling_index = self.__next_sibling_index()
-            self.__back()
+            next_sibling_index = self._next_sibling_index()
+            self._back()
         return self.current.children[next_sibling_index]
+
+
+class PostorderWalker(Walker):
+    def __init__(self, tree):
+        super().__init__(tree)
+        self.__fall()
+
+    def __next__(self):
+        result = self.current
+        if result is None:
+            raise StopIteration()
+        if not self.route:
+            self.current = None
+            return result
+        next_sibling_index = self._next_sibling_index()
+        self._back()
+        if next_sibling_index is not None:
+            self._walk_to(self.current.children[next_sibling_index])
+            self.__fall()
+        return result
+
+    def __fall(self):
+        while isinstance(self.current, Tree):
+            self._walk_to(self.current.children[0])
